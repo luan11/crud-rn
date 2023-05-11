c8428a6a0f37664fbd94824404c966660f4c84a6
Luan Novais <luaannovais@live.com>
2023-05-11 20:05:30 -0300

    feat: login

---
+++ src/Login.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  Dimensions,
+  Alert,
+} from 'react-native';
+
+const Login = () => {
+  const [username, setUsername] = useState(``);
+  const [password, setPassword] = useState(``);
+
+  const handleOnChangeUsername = (text: string) => setUsername(text);
+
+  const handleOnChangePassword = (pass: string) => setPassword(pass);
+
+  const signIn = () => {
+    if (username === `luanzera` && password === `12345`) {
+      Alert.alert(`Successfully logged!`);
+    } else {
+      Alert.alert(`Username and/or password incorrect!`);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Access page</Text>
+
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Username:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleOnChangeUsername}
+          value={username}
+        />
+      </View>
+
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>Password:</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry
+          onChangeText={handleOnChangePassword}
+          value={password}
+        />
+      </View>
+
+      <View style={styles.button}>
+        <Button title="Enter" onPress={signIn} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: `center`,
+  },
+  inputGroup: {
+    width: Dimensions.get(`screen`).width - 40,
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 4,
+    fontSize: 10,
+    padding: 10,
+  },
+  label: {
+    marginTop: 20,
+    marginBottom: 4,
+  },
+  button: {
+    width: Dimensions.get(`screen`).width - 40,
+    marginTop: 32,
+  },
+});
+
+export default Login;
